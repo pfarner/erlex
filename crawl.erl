dbabd9e9e077eacc3b3a5c2ec44837fade947683
Preston Pfarner <preston.pfarner@openx.com>
2016-11-05 13:09:30 -0700

    Converting crawl:print/1 to use crawl:map/2

--- crawl.erl
+++ crawl.erl
@@ -2,15 +2,9 @@
 -export([print/1,map/2]).
 
 print(Root) ->
-  io:format("~s~n", [Root]),
-  case file:list_dir(Root) of
-    {ok, Files} ->
-      lists:map(
-        fun(File) ->
-	  print(Root ++ "/" ++ File)
-	  end, Files);
-    Error -> Error
-  end.
+  map(Root, fun(File) ->
+    io:format("~s~n", [File])
+    end).
 
 map(Root, Function) ->
   Function(Root),
