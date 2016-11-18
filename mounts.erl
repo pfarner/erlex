947aa843b76f47c4e2b7f099bc260ca90834e893
Preston Pfarner <preston.pfarner@openx.com>
2016-11-18 00:42:35 -0800

    Now computing both mount points and types

--- mounts.erl
+++ mounts.erl
@@ -1,23 +1,48 @@
 -module(mounts).
--export([get_mounts/0,get_mount/1]).
+-export([get_mounts/0,get_types/0,get_mount/1,get_type/1]).
 
 get_mounts() ->
   Data = raw_read_file("/etc/mtab"),
   parse_mounts(binary:split(Data, [<<"\n">>], [global]), #{}).
 
+get_types() ->
+  Data = raw_read_file("/etc/mtab"),
+  parse_types(binary:split(Data, [<<"\n">>], [global]), #{}).
+
+% See also http://git.savannah.gnu.org/cgit/coreutils.git/tree/src/df.c
 get_mount(Path) ->
-  AbsPath = filename:absname(iolist_to_binary(Path)),
+  AbsPath = abspath(Path),
   try maps:get(AbsPath, get_mounts())
   catch error:bad_key ->
     case AbsPath of
-      "/" ->
-        throw("no_mount");
       <<"/">> ->
         throw("no_mount");
       _ ->
         get_mount(filename:dirname(AbsPath))
     end
   end.
+
+get_type(Path) ->
+  AbsPath = abspath(Path),
+  try maps:get(AbsPath, get_types())
+  catch error:bad_key ->
+    case AbsPath of
+      <<"/">> ->
+        throw("no_type");
+      _ ->
+        get_type(filename:dirname(AbsPath))
+    end
+  end.
+
+abspath(Path) ->
+  AbsPath = filename:absname(iolist_to_binary(Path)),
+  case binary:at(AbsPath, 0) of
+    $/ ->
+      AbsPath;
+    _  ->
+      throw(io_lib:format("absolute path not absolute: ~p became ~p", [Path, AbsPath]))
+  end.
+
 
 raw_read_file(Path) ->
   {ok, File} = file:open(Path, [read, binary]),
@@ -41,4 +66,12 @@ parse_mounts([Blob | Rest], Map) ->
   [Partition | [Mount | _]] = binary:split(Blob, [<<" ">>], [global]),
   parse_mounts(Rest, maps:put(Mount, Partition, Map));
 parse_mounts([], Map) ->
+  Map.
+
+parse_types([<<>> | Rest], Map) ->
+  Map;
+parse_types([Blob | Rest], Map) ->
+  [Partition | [Mount | [Type | _]]] = binary:split(Blob, [<<" ">>], [global]),
+  parse_types(Rest, maps:put(Mount, Type, Map));
+parse_types([], Map) ->
   Map.
